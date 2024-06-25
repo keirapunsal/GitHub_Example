a87383ef9ca76c3cd30655beae09a314855da09a
keirapunsal <keirapunsal4@gmail.com>
2024-06-25 09:44:08 -0700

    Initial Commit

---
+++ GitHub_Example/GitHub_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExampleApp.swift
+//  GitHub_Example
+//
+//  Created by Scholar on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
